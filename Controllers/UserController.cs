eb30d8f42e969847029a6b9599fbef0d1d57c869
Babatunde Mion <Fawazboluwatife7@gmail.com>
2024-07-04 16:58:15 +0100

    Added Location

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -172,13 +172,14 @@
 //        }
 //    }
 //}
-
 using HNG_WEB_API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
-using System;
+using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace YourNamespace.Controllers
 {
@@ -187,7 +188,7 @@ namespace YourNamespace.Controllers
     public class helloController : ControllerBase
     {
         private readonly HttpClient _httpClient;
-        private readonly IpApiClient _ipApiClient; 
+        private readonly IpApiClient _ipApiClient;
 
         public helloController(HttpClient httpClient, IpApiClient ipApiClient)
         {
@@ -200,10 +201,21 @@ namespace YourNamespace.Controllers
         {
             try
             {
+                // Decode the visitor_name parameter to handle URL-encoded quotation marks
+                visitor_name = HttpUtility.UrlDecode(visitor_name);
+
+                // Remove surrounding quotation marks, if any
+                if (!string.IsNullOrEmpty(visitor_name) && visitor_name.StartsWith("\"") && visitor_name.EndsWith("\""))
+                {
+                    visitor_name = visitor_name.Substring(1, visitor_name.Length - 2);
+                }
+
+                // Get the client's IP address
                 var ipAddress = HttpContext.Request.Headers["X-Forwarded-For"].FirstOrDefault()
                                 ?? HttpContext.Connection.RemoteIpAddress?.ToString();
                 var ipAddressWithoutPort = ipAddress?.Split(':')[0];
 
+                // Get the city from the IP address
                 var ipApiResponse = await _ipApiClient.Get(ipAddressWithoutPort, ct);
 
                 var response = new
@@ -230,7 +242,7 @@ namespace YourNamespace.Controllers
                 string temperature = weatherData["current"]["temp_c"].ToString();
 
                 // Create the result message
-                var resultMessage = $"hello, {visitor_name}!, you are in {response.City} and the temperature is {temperature} °C";
+                var resultMessage = $"Hello, {visitor_name}!, the temperature is {temperature} degrees Celcius  in {response.City}";
 
                 // Create an anonymous object to hold IP, City, and the result message
                 var result = new
